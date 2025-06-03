425f0616db9322948f90b67e5413868cd9d3ab9d
dennis.mathew <dennis.mathew@synchronoss.com>
2025-06-03 17:07:28 -0400

    merge main

--- src/main/java/com/propertytest/PropertyTest/PropertyTestApplication.java
+++ src/main/java/com/propertytest/PropertyTest/PropertyTestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PropertyTestApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Starting PropertyTestApplication...");
 		SpringApplication.run(PropertyTestApplication.class, args);
 	}
 
